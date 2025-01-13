a79db76771390b4904b6bd38abbb14e26efa2a1c
Deepak Dhage <deepakdhage111@gmail.com>
2025-01-13 23:07:01 +0530

    adding login logout views

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to the homepage after successful login
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
